dea132bdd5baddd735147be504a1339383bb6e77
Himanshu-kashyap <himanshus.kashyap0@gmail.com>
2021-05-07 16:06:47 +0530

    lesson 1

--- 1_1_OOP_basics.scala
+++ 1_1_OOP_basics.scala
@@ -1,7 +1,14 @@
 object OOP_basics extends App{
-    
+    var m = new Module2("Elephant",100)
+    println(m another "amar")
+
 }
 
-class Module2{
-        
+// class parameters are different from class fields
+class Module2(var name:String, var age:Int){ // using a var before a function will make it a 
+                                             // class field and hence callable using object
+        println(name) // side effects always execute on instantiation
+
+        def another(name:String):Unit = { println(s"this is $name talking") } 
+         
     }
