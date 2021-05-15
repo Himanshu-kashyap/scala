71d54fa5031ab162ffd75f83f2c1b60c76745452
Himanshu-kashyap <himanshus.kashyap0@gmail.com>
2021-05-15 13:39:10 +0530

    added inheritance 1

---
+++ 1_2_3_Inheritance_traits.scala
@@ -0,0 +1,53 @@
+object Inheritance_demo extends App{
+
+    
+    class Animal(feet:Int = 4,fur:Boolean){
+        def barks = false
+        def isADog = println("Not a Dog")
+        final var isAnimal = true
+        var type_var = "Reptile"
+        val type_val = "Reptile"
+        val horns = 2
+
+        //def this(fur:Boolean) = this(4,fur)
+    }
+
+    /*
+        correct syntax for inheritance is providing class constructors
+        for both parent and child class 
+    
+    */
+
+    class Dog(feet:Int,fur:Boolean,breed:String) extends Animal(feet,fur){
+        println("printing while instantiating")
+        println(feet+"\n"+fur+"\n"+breed+"\n")
+
+
+        // calling isADog which calls parent method not overriden yet
+        print("Before overriding ---> ")   // will call the function in same scope if declared 
+        isADog      // comment the overriden method in current Dog class to print parent value else will print curr val
+
+        // what happens after overriding the method
+        override def isADog = println("definitely a dog")
+        // try and call the super method for is a Dog
+        print("After overriding ---> ")
+        isADog
+
+        print("Calling super after overriding ---> ")
+        super.isADog
+
+        override def barks = true
+        override val type_val = "Mammal"
+        type_var = "Mammal" // mutable variable cannot be overriden but can be reassigned value
+        // comment above line to print parent's value
+        override val horns = 0
+
+        // override var isAnimal = false  // gives error as trying to override final method
+
+    }
+
+    val x:Animal = new Dog(4,true,"Pug")
+    // var y = new Animal
+    println(x.barks + "\t" + x.type_var + "\t" + x.horns + "\t" + x.type_val) 
+    
+}
--- 1_2_Scala_Objects.scala
+++ 1_2_Scala_Objects.scala
@@ -4,12 +4,42 @@
 // instead of using static to use class level values we use object in scala
 // 
 object Scala_Objs extends App{
+    
+    /*  
+     * 1 demonstation of object properties singleton 
+    */
+    
     object inner_test{
         var t = 20   // this is equivalent to using static int t= 20 in java aka class level functionality
     }
 
+
+    /*
+        use of companion class with object
+    */
+    
+    object Companion_Demo{
+        // this one is for class level functionality inside scala objs object
+        println("this should run on instance creation of inner object and will print only once since it's same instance everywhere")
+        var test = 2  // this is the alternative of static 
+
+        // using factory method for using this companion object and class structure with ease
+
+        def apply(name:String):Companion_Demo = return new Companion_Demo(name)
+
+    }
+
+    class Companion_Demo(var name:String){
+        
+        var test = 30 // inside class
+        println(s"this should print for $name")
+
+    }
+
+    // demo of class level fxnality
     println(inner_test.t)
 
+    // singleton instance
     var test1 = inner_test // notice the absense of new identifier indicating use of singleton instance
     var test2 = inner_test
 
@@ -18,15 +48,9 @@ object Scala_Objs extends App{
     }
 
 
-    var tt = new ThisClass()
-    tt.checkout
+    // using fxnality of both object and class with factory method
+    var inst_demo = Companion_Demo // prints 
+    var amar = Companion_Demo("Amar")
+    var akbar = Companion_Demo("Akbar")
 
-
-}
-
-class ThisClass(){
-    object checkout{
-        println("inside checkout")
-        var test = 2
-    }
 }
